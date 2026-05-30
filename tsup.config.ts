e04c6c8f8d83b61934be0add2614f3bc6b4ca7fa
Mike Nguyen <mike@datacore.vn>
2026-05-30 13:02:30 -0700

    ci: add tsup.config.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,2 +1,12 @@
 import { defineConfig } from 'tsup';
-export default defineConfig({ entry: ['src/index.ts'], format: ['esm'], dts: true, clean: true, sourcemap: true });
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['esm'],
+  dts: true,
+  clean: true,
+  sourcemap: true,
+  treeshake: true,
+  target: 'es2020',
+  outDir: 'dist',
+});
