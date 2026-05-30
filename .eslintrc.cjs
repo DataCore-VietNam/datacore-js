dbdfe328cde9a6b09f83ae47dd020a7fb5b0c888
Mike Nguyen <mike@datacore.vn>
2026-05-30 13:03:08 -0700

    ci: add .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,1 +1,16 @@
-module.exports = { root: true, parser: '@typescript-eslint/parser', plugins: ['@typescript-eslint'], extends: ['plugin:@typescript-eslint/recommended'], rules: { '@typescript-eslint/no-explicit-any': 'warn', 'no-empty': ['error', { allowEmptyCatch: false }] } };
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: { project: './tsconfig.json' },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'warn',
+    'no-empty': ['error', { allowEmptyCatch: false }],
+  },
+};
