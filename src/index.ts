2c4f07f211d5c2913e34207755ac8c1457a2ea77
Mike Nguyen <mike@datacore.vn>
2026-05-30 12:03:36 -0700

    chore: add src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { DataCore } from './client.js';
+export { Dataset } from './dataset.js';
+export type { Row } from './dataset.js';
+export { DataCoreError, AuthenticationError, NotFoundError, RateLimitError, ValidationError, ServerError } from './errors.js';
+export type { Domain, Product, DatasetStub, DatasetMetadata, Column, SearchResult, DataCoreClientOptions, FetchParams, FetchLike } from './types.js';
