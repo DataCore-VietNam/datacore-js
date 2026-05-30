731fb4bf18698e4c1b55964148b6d9f1c440ec49
Mike Nguyen <mike@datacore.vn>
2026-05-30 12:04:24 -0700

    chore: add .eslintrc.cjs

---
+++ .eslintrc.cjs
@@ -0,0 +1,1 @@
+module.exports = { root: true, parser: '@typescript-eslint/parser', plugins: ['@typescript-eslint'], extends: ['plugin:@typescript-eslint/recommended'], rules: { '@typescript-eslint/no-explicit-any': 'warn', 'no-empty': ['error', { allowEmptyCatch: false }] } };
